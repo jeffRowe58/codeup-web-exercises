a3251e92a02d4931c8166a015c54886f4857c71d
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-03-19 10:48:08 -0500

    more adjustment

--- js/external.js
+++ js/external.js
@@ -1,8 +1,12 @@
 "use strict";
+//Intro
 console.log("Hello from external Javascript");
+//Part 1
 alert("Welcome to my Website!");
+// Part 2
 var favcolor = prompt("What is your favorite color?");
 alert(favcolor + " is a cool color Bro!");
+//Part 3.1
 var rented = confirm("Did you rent any movies?")
 if(rented === true) {
     var moviePrice = 3;
@@ -14,6 +18,7 @@ if(rented === true) {
 }else {
     alert("You're boring!");
 }
+//Part 3.2
 alert("Pay time!");
 var google = 400;
 var amazon = 380;
@@ -23,15 +28,19 @@ var aP = parseFloat(prompt("How many hours did you work at Amazon?"));
 var fP = parseFloat(prompt("How many hours did you work at Facebook?"));
 var finalPay = ((gP * google) + (aP * amazon) + (fP * facebook))
 alert("Your hard earned money totals: $" + finalPay.toFixed(2));
-
+//Part 3.3
 var studentEnroll = confirm("Class starts at 9AM, can you attend?");
-var classMax = 26;
-var classSize = 23;
+if(studentEnroll === true){
+    var classMax = 26;
+    var classSize = 26;
+    classSize = classSize + 1
+}
 if(classSize <= classMax && studentEnroll === true){
     alert("See you on the first day!");
 }else {
     alert("Better luck next time.");
 }
+//Part 3.4
 alert("Welcome to ShopCo!");
 var permMem = confirm("Are you a Premium Member?");
 var userP = parseFloat(prompt("How many items are you picking up?"));
