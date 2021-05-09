77701464b67fe01c7323293818d49a631676d6ea
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-05-09 00:55:20 -0500

    final-ish project

---
+++ js/mapbox_maps_api.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const myPlaces = [
+    {
+        name: "Home Town",
+        location: [-96.6389, 33.1983],
+        why: "Cause I live here."
+    },
+    {
+        name: "Riskey's BBQ",
+        location: [-97.3454455, 32.7885852],
+        why: "Best BBQ I'VE ever had. Also can't beat $13.99 all you can eat beef ribs!",
+    },
+    {
+        name: "Jungle Burger",
+        location: [-96.63819, 33.20023],
+        why: "Mighty tasty. Big burgers. Not too expensive."
+    },
+    {
+        name: "Del Sur Mexican Grill",
+        location: [-96.683070, 33.173340],
+        why: "Taco are authentic and cheap for what you get."
+    }
+    ];
+
