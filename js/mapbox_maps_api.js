77283faf85fc9f647fac21d129fad47065d4df15
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-05-09 17:38:39 -0500

    added picture

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -7,19 +7,22 @@ const myPlaces = [
         why: "Cause I live here."
     },
     {
-        name: "Riskey's BBQ",
+        name: "Riscky's BBQ",
         location: [-97.3454455, 32.7885852],
         why: "Best BBQ I'VE ever had. Also can't beat $13.99 all you can eat beef ribs!",
+        img: "img/Logo-Risckys-BBQ.png"
     },
     {
         name: "Jungle Burger",
         location: [-96.63819, 33.20023],
-        why: "Mighty tasty. Big burgers. Not too expensive."
+        why: "Mighty tasty. Big burgers. Not too expensive.",
+        img: "img/JungleBurger.jpeg"
     },
     {
         name: "Del Sur Mexican Grill",
         location: [-96.683070, 33.173340],
-        why: "Taco are authentic and cheap for what you get."
+        why: "Taco are authentic and cheap for what you get.",
+        img: "img/DelSur.jpeg"
     }
     ];
 
