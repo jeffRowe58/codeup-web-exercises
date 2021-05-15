8733dec3a855af215fed36b2f44ca9f20b132fd8
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-05-15 08:48:48 -0500

    timer

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+        yearsOfExperience: 5
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+        yearsOfExperience: 6
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+        yearsOfExperience: 7
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+        yearsOfExperience: 8
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+        yearsOfExperience: 9
+    }
+];
+
+let languageArray = users.filter( language => (language.languages.length >= 3) ? language : "");
+console.log(languageArray);
+
+let emails = users.map(eaddress => eaddress.email);
+console.log(emails);
+
+let yearsAverage = users.reduce((a, b,c) => {
+    console.log(c = a.yearsOfExperience + b.yearsOfExperience);
+    return a + b / users.length;
+})
+console.log(yearsAverage);
+console.log(users.length);
