5ca002fbb4255f57cafc47002537f665df5b12dc
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-05-17 13:59:17 -0500

    map filter reduce complete

--- js/map-filter-reduce.js
+++ js/map-filter-reduce.js
@@ -38,15 +38,29 @@ const users = [
     }
 ];
 
-let languageArray = users.filter( language => (language.languages.length >= 3) ? language : "");
-console.log(languageArray);
+const languageArray = users.filter( language => (language.languages.length >= 3) ? language : "");
 
-let emails = users.map(eaddress => eaddress.email);
-console.log(emails);
 
-let yearsAverage = users.reduce((a, b,c) => {
-    console.log(c = a.yearsOfExperience + b.yearsOfExperience);
-    return a + b / users.length;
-})
-console.log(yearsAverage);
-console.log(users.length);
+const emails = users.map(eaddress => eaddress.email);
+
+
+const yearsAverage = users.reduce((a , b) =>
+     a + b.yearsOfExperience / users.length, 0);
+
+const emailLength = users.reduce((a, b)=>
+    a.length > b.email.length ? a : b.email, []);
+console.log(emailLength);
+
+const instructors = users.reduce((a, b, index, array) => {
+    if(index === array.length - 1){
+        a += b.name + '.'
+    }else {
+        a += b.name + ', ';
+    }
+         return a;
+},'Your instructors names are: ');
+
+
+// const uniqueLanguage = users.reduce((a, b, index) =>
+//    a.includes(b.languages) ? a : [...a, b.languages[index]], "");
+// console.log(uniqueLanguage);
