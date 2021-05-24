0938731881fad5e8949a34b55e8d4498b7b9f9f0
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-05-24 09:30:11 -0500

    morning warmup

---
+++ js/warmups.js
@@ -0,0 +1,14 @@
+'use strict';
+
+for(var i = 1; i <= 30; i ++){
+    if(i % 3 === 0 && i % 5 === 0){
+        document.getElementById('content').innerHTML += "FizzBuzz<br>";
+    }else if(i % 5 === 0){
+        document.getElementById('content').innerHTML += "Buzz<br>";
+
+    }else if (i % 3 === 0){
+        document.getElementById('content').innerHTML += "Fizz<br>";
+    }else{
+        document.getElementById('content').innerHTML += i + "<br>";
+    }
+}
