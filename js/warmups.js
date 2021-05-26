d2a63250b06ee4cbb2e591f5816aed9216476241
Jeffrey Rowe <jeffrey.r.rowe58@gmail.com>
2021-05-26 09:15:57 -0500

    daily warmup complete

--- js/warmups.js
+++ js/warmups.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// FizzBuzz
+
 for(var i = 1; i <= 30; i ++){
     if(i % 3 === 0 && i % 5 === 0){
         document.getElementById('content').innerHTML += "FizzBuzz<br>";
@@ -13,8 +15,15 @@ for(var i = 1; i <= 30; i ++){
     }
 }
 
+//return 7
+
  function seven(){
     return 7;
 }
-
 document.getElementById('content').innerHTML =  seven();
+
+//Times 5
+
+var timesFive = (x) => (typeof (x) === "number") ?  x * 5 : 0;
+
+document.getElementById('content').innerHTML +="<br>" + timesFive(12);
